72e9bd3706447bf8d13aa624274bf8a1cc93b71c
Atul-28 <137257234+Atul-28@users.noreply.github.com>
2023-06-30 10:44:11 +0530

    Add files via upload

---
+++ DAY-9/typecasting.js
@@ -0,0 +1,11 @@
+let myvar;
+myvar=String(43);
+document.write(myvar,"",typeof myvar);
+
+
+let d=new String(Date());
+console.log(d,typeof d);
+
+let arr=new String(Date());
+console.log(d,typeof arr);
+
